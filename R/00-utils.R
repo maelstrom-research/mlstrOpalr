6d7ef6d6af912f369478d6f7fce2e5d74884ddef
GuiFabre <guijoseph.fabre@gmail.com>
2023-06-20 13:26:54 -0400

    addition of opalr wrapper functions

---
+++ R/00-utils.R
@@ -0,0 +1,69 @@
+#' @title
+#' Call the help center for full documentation
+#'
+#' @description
+#' This function is a direct call to the documentation in the repository hosting
+#' the package. The user can access the description of the latest version of the
+#' package, the vignettes, and the list of functions.
+#' 
+#' @returns
+#' Nothing to be returned. The function opens a web page.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' mlstrOpalr_help()
+#' 
+#' }
+#'
+#' @importFrom utils browseURL
+#'
+#' @export
+mlstrOpalr_help <- function(){
+
+  browseURL("https://maelstrom-research.github.io/mlstrOpalr-documentation/")
+
+}
+
+#' @title
+#' Built-in tibble of allowed valueType values
+#'
+#' @description
+#' Provides a built-in tibble showing the list of allowed Opal valueType values
+#' and their corresponding R data types. This tibble is mainly used for 
+#' internal processes and programming.
+#'
+#' @details
+#' The valueType is a property of a variable and is required in certain 
+#' functions to determine the handling of the variables. The valueType refers 
+#' to the OBiBa-internal type of a variable. It is specified in a data 
+#' dictionary in a column `valueType` and can be associated with variables as 
+#' attributes. Acceptable valueTypes include 'text', 'integer', 'decimal', 
+#' 'boolean', datetime', 'date'). The full list of OBiBa valueType 
+#' possibilities and their correspondence with R data types are available using 
+#' [mlstrOpalr::valueType_list].
+#'
+#' @seealso
+#' [Opal documentation](https://opaldoc.obiba.org/en/dev/magma-user-guide/value/type.html)
+#'
+#' @format ## `tibble`
+#' A data frame with 12 rows and 6 columns:
+#' \describe{
+#'   \item{valueType}{data type as described in Opal}
+#'   \item{typeof}{data type provided by base::typeof}
+#'   \item{class}{data class provided by base::class}
+#'   \item{call}{function to transpose object according base::do.call function}
+#'   \item{toValueType}{ensemble data type as described in Opal}
+#'   \item{toTypeof}{ensemble data type provided by base::typeof}
+#'   \item{genericType}{ensemble data type which valueType belongs}
+#'   ...
+#' }
+#'
+#' @examples
+#' \dontrun{
+#'
+#' valueType_list
+#'
+#' }
+"valueType_list"
+
---
+++ README.md
@@ -0,0 +1,35 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# mlstrOpalr
+
+<!-- badges: start -->
+
+[![R-CMD-check](https://github.com/maelstrom-research/mlstrOpalr/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/maelstrom-research/mlstrOpalr/actions/workflows/R-CMD-check.yaml)
+<!-- badges: end -->
+
+# mlstrOpalr
+
+The goal of mlstrOpalr is to provide functions to support compatibility
+between Maelstrom R packages and Opal environment. Opal is the OBiBa
+application for data management, and Opal environments can be integrated
+with RStudio environments using the package `opalr`. The user must have
+adequate credentials to interact with a specific Opal environment.
+Please see [Opal documentation](https://opaldoc.obiba.org/en/dev/) for
+complete documentation.
+
+# Get started
+
+## Install the package
+
+``` r
+# To update the R package in your R environment you may first need to remove 
+# it, and use the exit command quit() to terminate the current R session.
+
+# To install the R package:
+install.packages('mlstrOpalr')
+library(mlstrOpalr) 
+
+#if you need help with the package, please use:
+mlstrOpalr_help()
+```
